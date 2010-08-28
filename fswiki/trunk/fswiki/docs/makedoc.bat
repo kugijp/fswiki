28ccc255a14c6136ec98e42253fca0d3f79fea6b
takezoe <takezoe@871d6764-1e22-0410-b313-a55050885396>
2010-08-28 14:53:26 +0000

    ドキュメントを更新。

--- fswiki/trunk/fswiki/docs/makedoc.bat
+++ fswiki/trunk/fswiki/docs/makedoc.bat
@@ -1,4 +1,4 @@
 @echo off
 rem HTMLƒtƒ@ƒCƒ‹‚É•ÏŠ·
-perl ../tools/wiki2html.pl "http://fswiki.org/wiki.cgi/docs?action=SOURCE&page=readme" -css=default.css -title=README > readme.html
-perl ../tools/wiki2html.pl "http://fswiki.org/wiki.cgi/docs?action=SOURCE&page=changes" -css=default.css -title=CHANGES > changes.html
+perl ../tools/wiki2html.pl "http://fswiki.sourceforge.jp/cgi-bin/wiki.cgi/docs?action=SOURCE&page=readme" -css=default.css -title=README > readme.html
+perl ../tools/wiki2html.pl "http://fswiki.sourceforge.jp/cgi-bin/wiki.cgi/docs?action=SOURCE&page=changes" -css=default.css -title=CHANGES > changes.html
