f1b1e7905bd457939452103b45a4452f5b3825a4
takezoe <takezoe@871d6764-1e22-0410-b313-a55050885396>
2010-11-21 16:02:57 +0000

    amazonbプラグインの機能を取り込みました。  * 携帯からのアクセスの場合はAmazonの携帯ページにリンクするようにした。  * インラインプラグイン化した。  * 第二引数にコメントを指定した場合は書影ではなくコメントにリンクをはるようにした。

--- plugin/amazon/Amazon.pm
+++ plugin/amazon/Amazon.pm
@@ -3,7 +3,7 @@ package plugin::amazon::Amazon;
 #
 # <p>指定した書籍の書影をamazonから取得して表示し、amazonの書評ページへリンクをはります。</p>
 # <pre>
-#   {{amazon aid}}
+#   {{amazon asin[,comment]}}
 # </pre>
 # <p>
 #   setup.dat に amazon_aid という定数を設定すると amazon のアソシエトID つきでリンクがはられます。
@@ -11,6 +11,9 @@ package plugin::amazon::Amazon;
 # <p>
 #   イメージが存在しないかどうか確認するためにamazonのサーバに接続しているので、
 #   プロキシ経由で外に出る必要がある場合は、プロキシの設定情報をsetup.datに設定しておく必要があります。
+# </p>
+# <p>
+#   comment 引数があたえられると、書影画像のかわりにその文字列からリンクをはります。
 # </p>
 #
 ###############################################################################
@@ -27,29 +30,46 @@ sub new {
 	return bless $self,$class;
 }
 #==============================================================================
-# パラグラフメソッド
+# インラインメソッド
 #==============================================================================
-sub paragraph {
-	my $self = shift;
-	my $wiki = shift;
-	my $item = shift;
+sub inline {
+	my $self    = shift;
+	my $wiki    = shift;
+	my $item    = shift;
+	my $comment = shift;
+	
 	$item = Util::escapeHTML($item);
-	my $noimg = 'http://images-jp.amazon.com/images/G/09/icons/books/comingsoon_books.gif';
+	my $buf;
+	my $link;
 	my $aid = $wiki->config('amazon_aid');
-	my $link = 'http://www.amazon.co.jp/exec/obidos/ASIN/' .  $item;
-	$link .= '/' . $aid if $aid;
-	my $image;
-	if($item =~ /^4/){
-		$image = "http://images-jp.amazon.com/images/P/$item.09.MZZZZZZZ.jpg";
+
+	if(Util::handyphone()){
+		if ($aid != '' ){
+			$link = 'http://www.amazon.co.jp/gp/aw/rd.html?uid=NULLGWDOCOMO&at='. $aid .'&a='. $item .'&dl=1&url=%2Fgp%2Faw%2Fd.html';
+		} else {
+			$link = 'http://www.amazon.co.jp/gp/aw/rd.html?uid=NULLGWDOCOMO&a='. $item .'&dl=1&url=%2Fgp%2Faw%2Fd.html';
+		}
 	} else {
-		$image = "http://images-jp.amazon.com/images/P/$item.01.MZZZZZZZ.jpg";
+		$link = 'http://www.amazon.co.jp/exec/obidos/ASIN/' .  $item;
+		$link .= '/' . $aid . '/ref=nosim' if $aid;
+	}
+
+	if($comment eq ""){
+		my $noimg = 'http://images-jp.amazon.com/images/G/09/icons/books/comingsoon_books.gif';
+		my $image;
+		my $response;
+		for my $num ( '09','01' ) {
+			$image = "http://images-jp.amazon.com/images/P/$item.$num.MZZZZZZZ.jpg";
+			$response = &Util::get_response($wiki,$image);
+			last unless (length($response) < 1024);
+		}
+		$image = $noimg if (length($response) < 1024);
+		$buf = '<img src="'.$image.'">';
+	} else{
+		$buf = Util::escapeHTML($comment);
 	}
 	
-	my $response = &Util::get_response($wiki,$image);
-	$image = $noimg if (length($response) < 1024);
-	
-	my $buf = "<div class=\"amazon\"><a href='$link'><img src='$image'></a></div>";
-	return $buf;
+	return '<span class="amazonb"><a href="'.$link.'">'.$buf.'</a></span>';
 }
 
 1;
