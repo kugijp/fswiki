3edb273c01c761467886a9d938286ae12b3a1659
takezoe <takezoe@871d6764-1e22-0410-b313-a55050885396>
2011-07-22 04:09:50 +0000

    現在のページ名を表示するpagenameプラグインを追加。

--- fswiki/trunk/fswiki/plugin/info/Install.pm
+++ fswiki/trunk/fswiki/plugin/info/Install.pm
@@ -12,6 +12,7 @@ sub install{
 	
 	$wiki->add_inline_plugin("counter","plugin::info::Counter","WIKI");
 	$wiki->add_inline_plugin("lastmodified","plugin::info::LastModified","WIKI");
+	$wiki->add_inline_plugin("pagename","plugin::info::PageName","WIKI");
 	$wiki->add_paragraph_plugin("outline","plugin::info::Outline","HTML");
 	$wiki->add_paragraph_plugin("todayslink","plugin::info::TodaysLink","HTML");
 	
