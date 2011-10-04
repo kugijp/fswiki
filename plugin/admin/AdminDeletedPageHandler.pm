42dabbd0af9d30ac1f4398beb48dfdfae9531e33
takezoe <takezoe@871d6764-1e22-0410-b313-a55050885396>
2011-10-04 17:21:14 +0000

    削除されたページを完全に削除できるようにした。

--- plugin/admin/AdminDeletedPageHandler.pm
+++ plugin/admin/AdminDeletedPageHandler.pm
@@ -19,12 +19,21 @@ sub new {
 }
 
 #==============================================================================
+# 削除されたページを記録するファイルのパスを返す関数
+#==============================================================================
+sub _get_deleted_file {
+	my $wiki = shift;
+	return $wiki->config('log_dir')."/".$DELETED_FILE
+}
+
+#==============================================================================
 # ページの削除時に呼び出される
 #==============================================================================
 sub hook {
 	my $self = shift;
 	my $wiki = shift;
-	open(FILE, ">>".$wiki->config('log_dir')."/".$DELETED_FILE);
+	open(FILE, ">>".&_get_deleted_file($wiki));
+	binmode(FILE);
 	print FILE $wiki->get_CGI()->param("page")."\t".time()."\n";
 	close(FILE);
 }
@@ -38,6 +47,11 @@ sub do_action {
 	my $cgi   = $wiki->get_CGI;
 	if($cgi->param('revert')){
 		$self->revert($wiki);
+		$wiki->redirectURL($wiki->create_url({ action=>"ADMINDELETED"}));
+	}
+	if($cgi->param('forget')){
+		$self->forget($wiki);
+		$wiki->redirectURL($wiki->create_url({ action=>"ADMINDELETED"}));
 	}
 	return $self->deleted_page_list($wiki);
 }
@@ -51,6 +65,52 @@ sub revert {
 	my @pages = $wiki->get_CGI()->param('pages');
 	foreach my $page (@pages){
 		$wiki->save_page($page, $wiki->get_backup($page, 0));
+	}
+}
+
+#==============================================================================
+# ページを完全に削除
+#==============================================================================
+sub forget {
+	my $self  = shift;
+	my $wiki  = shift;
+	my @selected_pages = $wiki->get_CGI()->param('pages');
+	my $deleted_file = &_get_deleted_file($wiki);
+	
+	# 削除ページを記録するファイルから選択されたページを削除
+	Util::file_lock($deleted_file);
+	open(FILE, $deleted_file);
+	my $pages = {};
+	my $buf = "";
+	while(my $LINE = <FILE>){
+		my ($page, $mod) = split(/\t/, Util::trim($LINE));
+		unless($wiki->page_exists($page)){
+			my $selected = 0;
+			foreach my $selected_page (@selected_pages){
+				if($page eq $selected_page){
+					$selected = 1;
+					last;
+				}
+			}
+			if($selected == 0){
+				$buf .= $page."\t".$mod."\n";
+			}
+		}
+	}
+	close(FILE);
+	
+	open(FILE, ">".$deleted_file);
+	binmode(FILE);
+	print FILE $buf;
+	close(FILE);
+	Util::file_unlock($deleted_file);
+
+	# バックアップファイルも削除
+	foreach my $selected_page (@selected_pages){
+		my @backup_files = glob(&Util::make_filename($wiki->config('backup_dir'), &Util::url_encode($selected_page), "*"));
+		foreach my $backup_file (@backup_files){
+			unlink($backup_file);
+		}
 	}
 }
 
@@ -73,10 +133,10 @@ sub deleted_page_list {
 		</tr>
 	|;
 	
-	open(FILE, $wiki->config('log_dir')."/".$DELETED_FILE);
+	open(FILE, &_get_deleted_file($wiki));
 	my $pages = {};
 	while(my $LINE = <FILE>){
-		my ($page, $mod) = split(/\t/, $LINE);
+		my ($page, $mod) = split(/\t/, Util::trim($LINE));
 		unless($wiki->page_exists($page)){
 			$pages->{$page} = $mod;
 		}
@@ -95,7 +155,8 @@ sub deleted_page_list {
 	
 	$buf .= qq|</table>
 		<input type="hidden" name="action" value="ADMINDELETED">
-		<input type="submit" name="revert" value="チェックしたページを復元する">
+		<input type="submit" name="revert" value="復元">
+		<input type="submit" name="forget" value="完全に削除">
 	</form>|;
 	
 	$wiki->set_title("削除されたページ");
